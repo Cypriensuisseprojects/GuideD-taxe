47a0a08ebfde7599c18e8fb67aa084db93b02597
Cypriensuisseprojects <cy.liance@sezane.com>
2026-03-22 00:43:27 +0100

    Add setStaticParamsLocale to i18n server export

--- src/i18n/server.ts
+++ src/i18n/server.ts
@@ -2,8 +2,8 @@
 import { createI18nServer } from 'next-international/server';
 import { locales, defaultLocale } from './config';
 
-
-export const { getI18n, getScopedI18n, getCurrentLocale } = createI18nServer({
+// J'ai ajouté "setStaticParamsLocale" à la fin de cette ligne 👇
+export const { getI18n, getScopedI18n, getCurrentLocale, setStaticParamsLocale } = createI18nServer({
   [defaultLocale]: () => import('./fr'), // Charge la locale par défaut
   en: () => import('./en'), // Charge les autres locales
   de: () => import('./de'),
@@ -11,6 +11,7 @@ export const { getI18n, getScopedI18n, getCurrentLocale } = createI18nServer({
   ja: () => import('./ja'),
   'zh-CN': () => import('./zh-CN'),
 });
+
 export function getStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
